713de3a21dfc33564d6da3d49962de158fb265a2
vlad <prosto.acc.vlada@gmail.com>
2024-05-07 13:09:52 +0300

    created fe

---
+++ movie_review_fe/movie-review/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import api from "./api/axiosConfig"
+import {useState, useEffect} from 'react'
+function App() {
+
+  const [movies, setMovies] = useState();
+
+  const getMovies = async() =>{
+    try{
+      const response = await api.get("/api/1.0/movies")
+
+      setMovies(response.data);
+      console.log(response.data);
+
+    }catch(err){
+      console.log(err);
+    }
+    
+  }
+
+  useEffect(()=>{
+    getMovies();
+  },[])
+
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
